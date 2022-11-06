dd8248cf376c4de5cbd25e3bcb1ec9b9da8adc7a
rakeshs10 <rsahano999@gmail.com>
2022-11-06 13:30:19 +0530

    my first commit

---
+++ addAandB.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class addAandB {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int a=scn.nextInt();
+        int b=scn.nextInt();
+        int c=a+b; 
+        System.out.println(c);
+    }
+}
