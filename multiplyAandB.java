f7e09ee851c01abbc062d19db85aea074b34c796
rakeshs10 <rsahano999@gmail.com>
2022-11-06 14:37:06 +0530

    my first multiply

--- multiplyAandB.java
+++ multiplyAandB.java
@@ -1,12 +1,10 @@
 import java.util.*;
-public class addAandB {
+public class multiplyAandB {
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
-        System.out.println("enter value of a");
         int a=scn.nextInt();
-        System.out.println("enter value of b");
         int b=scn.nextInt();
-        int c=a+b; 
+        int c=a*b; 
         System.out.println(c);
     }
 }
